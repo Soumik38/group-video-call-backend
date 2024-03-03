1c7b9891bf8ae75bddf7746db3973d770b493a85
Soumik Saha <soumiksahaasansol@gmail.com>
2024-03-03 21:51:19 +0530

    cors test 2

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ const PORT = process.env.PORT || 4000
 const path = require('path')
 
 let socketList = {}
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(function (req,res,next){
-  res.header("Access-Control-Allow-Origin","*")
-  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
+const cors=require('cors')
+app.use(cors())
+
+// app.use(function (req,res,next){
+//   res.header("Access-Control-Allow-Origin","*")
+//   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept")
+//   next()
+// })
 
 
 
