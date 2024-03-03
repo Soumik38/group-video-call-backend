2225746f4f745a5ab82ef4ffcf19320b015d0999
Soumik Saha <soumiksahaasansol@gmail.com>
2024-03-03 21:17:43 +0530

    cors test 1

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ let socketList = {}
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-
+app.use(function (req,res,next){
+  res.header("Access-Control-Allow-Origin","*")
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
 
 
